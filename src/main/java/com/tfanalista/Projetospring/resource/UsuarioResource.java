501257ce0ba4be9f29314b81dbf29c98ca651daa
Thiago Felipe <thiagofelipe_analistati@outlook.com>
2021-04-05 21:19:24 -0300

    Camada de Serviço e registro de componentes

--- src/main/java/com/tfanalista/Projetospring/resource/UsuarioResource.java
+++ src/main/java/com/tfanalista/Projetospring/resource/UsuarioResource.java
@@ -1,22 +1,38 @@
 package com.tfanalista.Projetospring.resource;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tfanalista.Projetospring.entidades.Usuario;
+import com.tfanalista.Projetospring.service.UsuarioService;
+
+
 
 
 @RestController //implementação do rest 
 @RequestMapping (value = "/usuarios")
 public class UsuarioResource {
 	
+	@Autowired
+	private UsuarioService service;
 	@GetMapping
-	public ResponseEntity<Usuario> findall(){
-		Usuario u = new Usuario(1L,"Thiago","thiago@fel", "987526534", "123456");
-				// Usuario(1L,"Thiago","thiago@fel", "987526534", "123456");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<Usuario>> findall(){
+		
+		List<Usuario> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Usuario> FindById(@PathVariable Long id){ 
+		// @PathVariable para o spring aceitar no get
+		Usuario obj  = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 	
 }
---
+++ src/main/java/com/tfanalista/Projetospring/service/UsuarioService.java
@@ -0,0 +1,30 @@
+package com.tfanalista.Projetospring.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tfanalista.Projetospring.entidades.Usuario;
+import com.tfanalista.Projetospring.repositories.UsuarioRepository;
+
+@Service//registrando como componente do frame spring
+public class UsuarioService {
+	
+	
+	@Autowired
+	private UsuarioRepository userRepository;
+	// metodo para buscar todos os usuários
+	public List<Usuario> findAll(){
+		return userRepository.findAll();
+
+	}
+	// metodo para buscar o usuário pelo id do tipo Long
+	public Usuario findById(Long id) {
+		
+		Optional<Usuario> obj = userRepository.findById(id);
+		return obj.get();
+		
+	}
+}
