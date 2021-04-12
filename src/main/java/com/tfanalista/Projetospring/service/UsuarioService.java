6ce7868444ac94aca8c05872e12f796fcf5c26c2
Thiago Felipe <thiagofelipe_analistati@outlook.com>
2021-04-12 12:36:29 -0300

    Inserção do Delete Usuário

--- src/main/java/com/tfanalista/Projetospring/service/UsuarioService.java
+++ src/main/java/com/tfanalista/Projetospring/service/UsuarioService.java
@@ -31,4 +31,8 @@ public class UsuarioService {
 	public Usuario Insert(Usuario obj) {
 		return userRepository.save(obj);
 	}
+	
+	public void delete(Long id) {
+		userRepository.deleteById(id);
+	}
 }
