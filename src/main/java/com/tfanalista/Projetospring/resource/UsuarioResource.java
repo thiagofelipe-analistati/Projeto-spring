3e556edc63b6cec048fb3dd2b02edb68f2365349
Thiago Felipe <thiagofelipe_analistati@outlook.com>
2021-04-12 11:45:59 -0300

    Metodo POST insert

--- src/main/java/com/tfanalista/Projetospring/resource/UsuarioResource.java
+++ src/main/java/com/tfanalista/Projetospring/resource/UsuarioResource.java
@@ -1,13 +1,17 @@
 package com.tfanalista.Projetospring.resource;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.tfanalista.Projetospring.entidades.Usuario;
 import com.tfanalista.Projetospring.service.UsuarioService;
@@ -33,6 +37,13 @@ public class UsuarioResource {
 		// @PathVariable para o spring aceitar no get
 		Usuario obj  = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	@PostMapping
+	public ResponseEntity<Usuario> Insert(@RequestBody Usuario obj){
+		obj = service.Insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 	
 }
